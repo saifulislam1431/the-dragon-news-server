dff7650371cb7825b608654cf37a76e7e450c9d9
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-20 23:36:42 +0600

    Filtered news by category

--- index.js
+++ index.js
@@ -11,20 +11,35 @@ const categories = require('./data/categories.json');
 
 const news = require('./data/allNews.json');
 
-app.get('/' ,(req , res)=>{
+app.get('/', (req, res) => {
     res.send("The Dragon Data Server")
 });
-app.get('/categories' , (req , res)=>{
+
+
+app.get('/categories', (req, res) => {
     res.send(categories)
 });
-app.get('/news' , (req , res)=>{
+
+
+app.get('/news', (req, res) => {
     res.send(news)
 });
-app.get('/news/:id' , (req,res)=>{
+
+
+app.get('/news/:id', (req, res) => {
     const id = req.params.id;
-    const selectedNews = news.find(n=> n._id === id);
+    const selectedNews = news.find(n => n._id === id);
     res.send(selectedNews);
-})
-app.listen(port,()=>{
+});
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const filteredNews = news.filter(n=>n.category_id === id);
+    res.send(filteredNews);
+});
+
+
+app.listen(port, () => {
     console.log(`This server lister on port ${port}`);
 })
+
