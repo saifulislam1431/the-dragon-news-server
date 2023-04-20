c674355d512b51fdf7bd268462512e964f37a3e0
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-20 23:24:24 +0600

    All news json

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 
+const news = require('./data/allNews.json');
+
 app.get('/' ,(req , res)=>{
     res.send("The Dragon Data Server")
-})
+});
 app.get('/categories' , (req , res)=>{
     res.send(categories)
-})
+});
+app.get('/news' , (req , res)=>{
+    res.send(news)
+});
 app.listen(port,()=>{
     console.log(`This server lister on port ${port}`);
 })
