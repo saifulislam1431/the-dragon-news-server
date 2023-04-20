9f85d571d21a95478a0bd79f4f0210e58d2f0a43
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-20 23:32:17 +0600

    Get new by id

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.get('/categories' , (req , res)=>{
 app.get('/news' , (req , res)=>{
     res.send(news)
 });
+app.get('/news/:id' , (req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n=> n._id === id);
+    res.send(selectedNews);
+})
 app.listen(port,()=>{
     console.log(`This server lister on port ${port}`);
 })
